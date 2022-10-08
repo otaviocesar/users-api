e4c26fbb7b3f63f12d0c597b189ffe363f689726
Otavio Cesar <o.caesar@hotmail.com>
2022-10-08 20:00:32 -0300

    feat: Implementando UserRepository

--- src/main/java/com/mlb/usersapi/adapters/outbound/repository/UserRepository.java
+++ src/main/java/com/mlb/usersapi/adapters/outbound/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.mlb.usersapi.adapters.outbound.repository;
 
-public interface UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mlb.usersapi.adapters.inbound.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
