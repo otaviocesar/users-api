87df48d523e68f638a4af934c52917fa8661e570
Otavio Cesar <o.caesar@hotmail.com>
2022-10-08 19:09:22 -0300

    feat: Implementando domínio User

--- src/main/java/com/mlb/usersapi/application/core/domain/User.java
+++ src/main/java/com/mlb/usersapi/application/core/domain/User.java
@@ -1,5 +1,56 @@
 package com.mlb.usersapi.application.core.domain;
 
+import java.time.LocalDate;
+
 public class User {
-    
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 }
