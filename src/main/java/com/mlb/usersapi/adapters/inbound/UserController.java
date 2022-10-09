8c042ab688d04881a29db023b798d473f569bbf8
Otavio Cesar <o.caesar@hotmail.com>
2022-10-09 15:01:28 -0300

    docs: Adicionando @Operation em UserController

--- src/main/java/com/mlb/usersapi/adapters/inbound/UserController.java
+++ src/main/java/com/mlb/usersapi/adapters/inbound/UserController.java
@@ -6,23 +6,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import com.mlb.usersapi.adapters.inbound.dtos.UserDTO;
 import com.mlb.usersapi.adapters.inbound.mappers.UserDTOToUserMapper;
 import com.mlb.usersapi.application.core.domain.User;
+import com.mlb.usersapi.application.core.exception.ErrorResponse;
 import com.mlb.usersapi.application.ports.primary.SaveUserServicePort;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.*;
+import io.swagger.v3.oas.annotations.media.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Users", description = "Endpoint group to create, list, update and delete users")
 @AllArgsConstructor
 public class UserController {
 
     private final SaveUserServicePort saveUserServicePort;
 
     private final UserDTOToUserMapper userDTOToUserMapper;
+    
 
-    @PostMapping
+    @Operation(
+        summary = "Add a user",
+        description = "This operation saves a new record with the user information",
+        tags = {"users"},
+        responses = {
+            @ApiResponse(responseCode = "201", description = "Created."),
+            @ApiResponse(responseCode = "400", description = "Bad Request.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "409", description = "Conflit.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+        }
+    )
+    @PostMapping(
+		consumes = MediaType.APPLICATION_JSON_VALUE
+	)
     public ResponseEntity<User> save(@RequestBody @Valid UserDTO saveUserDTO) {
         User user = saveUserServicePort.save(userDTOToUserMapper.mapper(saveUserDTO));
         return new ResponseEntity<>(user, HttpStatus.CREATED);
