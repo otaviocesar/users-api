fda0136302ab92e65e4480b7249611598dfcf3eb
Otavio Cesar <o.caesar@hotmail.com>
2022-10-08 21:06:43 -0300

    refactor: Removendo implementações não utilizadas

--- src/main/java/com/mlb/usersapi/UsersApiApplication.java
+++ src/main/java/com/mlb/usersapi/UsersApiApplication.java
@@ -2,20 +2,12 @@ package com.mlb.usersapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class UsersApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApiApplication.class, args);
 	}
-
-	@GetMapping("/")
-	public String index() {
-		return "Olá Mundo!";
-	}
-
 }
