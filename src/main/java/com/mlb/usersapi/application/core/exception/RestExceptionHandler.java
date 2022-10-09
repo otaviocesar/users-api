37c62f8920efc55a3b003b19747b061462addf7c
Otavio Cesar <o.caesar@hotmail.com>
2022-10-09 15:00:34 -0300

    docs: Adicionando OpenApiConfig e descrições

--- src/main/java/com/mlb/usersapi/application/core/exception/RestExceptionHandler.java
+++ src/main/java/com/mlb/usersapi/application/core/exception/RestExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         List<ErrorObject> errors = getErrors(ex);
         ErrorResponse errorResponse = getErrorResponse(ex, status, errors);
