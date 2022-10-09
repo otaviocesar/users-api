52d0cbd36c0c98fbff0386e43aad8a590fa7c023
Otavio Cesar <o.caesar@hotmail.com>
2022-10-08 21:07:18 -0300

    fix: Ajustando SaveUserService

--- src/main/java/com/mlb/usersapi/application/core/services/SaveUserService.java
+++ src/main/java/com/mlb/usersapi/application/core/services/SaveUserService.java
@@ -2,17 +2,18 @@ package com.mlb.usersapi.application.core.services;
 
 import com.mlb.usersapi.application.core.domain.User;
 import com.mlb.usersapi.application.ports.primary.SaveUserServicePort;
+import com.mlb.usersapi.application.ports.secondary.SaveUserPort;
 
 public class SaveUserService implements SaveUserServicePort {
 
-    private final SaveUserServicePort saveUserServicePort;
+    private final SaveUserPort saveUserPort;
 
-    public SaveUserService(SaveUserServicePort saveUserServicePort) {
-        this.saveUserServicePort = saveUserServicePort;
+    public SaveUserService(SaveUserPort saveUserPort) {
+        this.saveUserPort = saveUserPort;
     }
 
     @Override
     public User save(User user) {
-        return saveUserServicePort.save(user);
+        return saveUserPort.save(user);
     }
 }
