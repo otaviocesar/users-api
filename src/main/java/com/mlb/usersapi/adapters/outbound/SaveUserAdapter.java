c9fdb224281d820477741ea40e42fb0e135d7646
Otavio Cesar <o.caesar@hotmail.com>
2022-10-09 23:46:51 -0300

    feat: Implementando validação de idade

--- src/main/java/com/mlb/usersapi/adapters/outbound/SaveUserAdapter.java
+++ src/main/java/com/mlb/usersapi/adapters/outbound/SaveUserAdapter.java
@@ -3,12 +3,17 @@ package com.mlb.usersapi.adapters.outbound;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import com.mlb.usersapi.adapters.inbound.entity.UserEntity;
 import com.mlb.usersapi.adapters.inbound.mappers.UserEntityToUserMapper;
 import com.mlb.usersapi.adapters.inbound.mappers.UserToUserEntityMapper;
 import com.mlb.usersapi.adapters.outbound.repository.UserRepository;
 import com.mlb.usersapi.application.core.domain.User;
+import com.mlb.usersapi.application.core.exception.BadRequestException;
 import com.mlb.usersapi.application.core.exception.ConflictException;
 import com.mlb.usersapi.application.ports.secondary.SaveUserPort;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 import javax.transaction.Transactional;
 
@@ -26,12 +31,28 @@ public class SaveUserAdapter implements SaveUserPort {
     @Transactional
     public User save(User user) {
         var userEntity = userToUserEntityMapper.mapper(user);
+        checkExistsByCpf(userEntity);
+        checkExistsByEmail(userEntity);
+        checkUserAge(userEntity);
+        return userEntityToUserMapper.mapper(userRepository.save(userEntity));
+    }
+
+    public void checkExistsByCpf(UserEntity userEntity) {
         if (userRepository.existsByCpf(userEntity.getCpf())){
             throw new ConflictException("User with cpf " + userEntity.getCpf() + " already exists.");
         }
+    }
+
+    public void checkExistsByEmail(UserEntity userEntity) {
         if (userRepository.existsByEmail(userEntity.getEmail())){
             throw new ConflictException("User with email " + userEntity.getEmail() + " already exists.");
         }
-        return userEntityToUserMapper.mapper(userRepository.save(userEntity));
+    }
+
+    public void checkUserAge(UserEntity userEntity) {
+        int userAge = Period.between(userEntity.getBirthDate(), LocalDate.now()).getYears();
+        if (userAge < 18){
+            throw new BadRequestException("User is " + userAge + " years old. Must be over 18 years old.");
+        }
     }
 }
--- src/main/java/com/mlb/usersapi/adapters/outbound/UpdateUserAdapter.java
+++ src/main/java/com/mlb/usersapi/adapters/outbound/UpdateUserAdapter.java
@@ -9,10 +9,13 @@ import com.mlb.usersapi.adapters.inbound.mappers.UserEntityToUserMapper;
 import com.mlb.usersapi.adapters.inbound.mappers.UpdateUserMapper;
 import com.mlb.usersapi.adapters.outbound.repository.UserRepository;
 import com.mlb.usersapi.application.core.domain.User;
+import com.mlb.usersapi.application.core.exception.BadRequestException;
 import com.mlb.usersapi.application.core.exception.ConflictException;
 import com.mlb.usersapi.application.core.exception.NotFoundException;
 import com.mlb.usersapi.application.ports.secondary.UpdateUserPort;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -34,10 +37,18 @@ public class UpdateUserAdapter implements UpdateUserPort {
         if (!userOptional.isPresent()) {
             throw new NotFoundException("User " + id + " does not exist.");
         }
+        checkUserAge(user);
         checkCPFLinkedToAnotherUser(id, user);
         checkEmailLinkedToAnotherUser(id, user);
 
         return userEntityToUserMapper.mapper(userRepository.save(updateUserMapper.mapper(userOptional, user)));
+    }
+
+    public void checkUserAge(User user) {
+        int userAge = Period.between(user.getBirthDate(), LocalDate.now()).getYears();
+        if (userAge < 18){
+            throw new BadRequestException("User is " + userAge + " years old. Must be over 18 years old.");
+        }
     }
 
     public void checkCPFLinkedToAnotherUser(Long id, User user) {
